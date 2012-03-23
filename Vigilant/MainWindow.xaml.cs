6bebe7ceeac3b66f887003d3a6ccace67763e073
Andrew Chaa <andrew.chaa@yahoo.co.uk>
2012-03-23 20:01:44 +0000

    WatchErrors

---
+++ Vigilant/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Growl.Connector;
+
+namespace Vigilant
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            var growlConnector = new GrowlConnector();
+            var application = new Growl.Connector.Application("Hello, growl!");
+            var notificationType = new NotificationType("sample", "Test");
+            growlConnector.Register(application, new [] {notificationType});
+
+            var notification = new Notification(application.Name, notificationType.Name, "1", "Test", "Hello there!");
+            growlConnector.Notify(notification);
+        }
+
+    }
+}
---
+++ WatchErrors/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using System.Text.RegularExpressions;
+
+namespace WatchErrorsTest
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void General_Information_Is_Splitted_By_Hr_Tag()
+        {
+            string log = File.ReadAllText(@"..\..\web_20120322.htm");
+            var messages = log.Split(new [] {"<hr/>"}, StringSplitOptions.RemoveEmptyEntries);
+
+            Assert.That(messages.Length, Is.EqualTo(3));
+        }
+    }
+}
